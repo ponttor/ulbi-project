b1cd1a8d739b1bda25f6ce9709d6e28c974cee4e
Alexander Illarionov <alex@heimstudio.ru>
2023-07-09 18:36:12 +0200

    13 Svg loader. File loader. Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,21 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
- export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] { 
+ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+
+  const fileLoader = {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        };
 
   const typescriptLoader = {
     test: /\.tsx?$/,
@@ -36,6 +50,8 @@ import { BuildOptions } from './types/config';
   };
 
   return [
+    svgLoader,
+    fileLoader,
     typescriptLoader,
     babelLoader,
     cssLoader
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,11 @@ import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 
 export default function App() {
-  const {theme, toggleTheme} = useTheme();
+  const {theme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>TOGGLE</button>
       <AppRouter />
     </div>
   )
