9874a6b20a89dfd6aae9eedd024566f8a8cfab0f
Alexander Illarionov <alex@heimstudio.ru>
2023-09-16 17:16:45 +0200

    refresh style

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, { FC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './AppLink.module.scss'
 import { Link, LinkProps } from 'react-router-dom';
@@ -6,6 +6,7 @@ import { Link, LinkProps } from 'react-router-dom';
 export enum AppLinkTheme {
   PRIMARY = 'primary',
   SECONDARY = 'secondary',
+  RED = 'red',
 }
 
 interface AppLinkProps extends LinkProps {
@@ -25,7 +26,7 @@ const AppLink: FC<AppLinkProps> = (props) => {
   return (
     <Link
       to={to}
-      className = {classNames(cls.AppLink, {}, [className, cls[theme]])}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
       {...otherProps}
     >
       {children}
